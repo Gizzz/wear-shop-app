770ae4b5cb965bcc274a7591c03cfb60eea35f66
Gizzz <bulatkhusainov@yandex.ru>
2017-03-15 22:52:44 +0300

    create index page, add config to display index page

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.set('port', (process.env.PORT || 3000));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
-  response.send("Hello wrld!");
+  response.sendFile(`${__dirname}/public/html/index.html`);
 });
+
+
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
