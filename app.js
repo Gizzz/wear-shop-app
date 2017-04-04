5227dd0863244bda7671a98cff0447be9e42ed7d
Gizzz <bulatkhusainov@yandex.ru>
2017-04-04 20:53:24 +0300

    add eslint config

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/public/html/index.html`);
+	res.sendFile(`${__dirname}/public/html/index.html`);
 });
 
 app.get("/list/:list_name", (req, res) => {
   // res.send("list requested: " + req.params.list_name);
 
-  res.sendFile(`${__dirname}/public/html/list.html`);
+	res.sendFile(`${__dirname}/public/html/list.html`);
 });
 
 
-app.set('port', (process.env.PORT || 3000));
+app.set("port", (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function() {
+	// eslint-disable-next-line no-console
+	console.log("Node app is running on port", app.get("port"));
 });
