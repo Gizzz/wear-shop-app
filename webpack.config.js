982a30b2f78f13aa63fed10ec78f777d2e37c4d9
Gizzz <bulatkhusainov@yandex.ru>
2017-04-10 19:44:07 +0300

    add Webpack and Babel integration

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+	context: __dirname,
+  entry: './frontend/App.jsx',
+  devtool: 'source-map',
+	output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+	module: {
+		rules: [
+			{ 
+				test: /\.js(x)?$/, 
+				exclude: /node_modules/, 
+				loader: "babel-loader" 
+			}
+		]
+	}
+};
