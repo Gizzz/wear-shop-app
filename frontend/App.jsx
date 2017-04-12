8d45fc860eda9023eba488ae8d2a0f5418d686cd
Gizzz <bulatkhusainov@yandex.ru>
2017-04-12 22:10:27 +0300

    eslint: add settings for jsx, fix warnings, add ignores

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import Header from "./Header.jsx";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
 	context: __dirname,
-  entry: './frontend/App.jsx',
-  devtool: 'source-map',
+	entry: "./frontend/App.jsx",
+	devtool: "source-map",
 	output: {
-    path: path.join(__dirname, '/public'),
-    filename: 'bundle.js'
-  },
+		path: path.join(__dirname, "/public"),
+		filename: "bundle.js"
+	},
 	module: {
 		rules: [
 			{ 
