3ce9530b7d4d2c2c91f790ac47e4f74230c1bc20
Gizzz <bulatkhusainov@yandex.ru>
2017-04-16 20:12:02 +0300

    make node app aware of client-side routing

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
 	res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.get("/list/:list_name", (req, res) => {
-  // res.send("list requested: " + req.params.list_name);
-
-	res.sendFile(`${__dirname}/public/html/list.html`);
-});
+// app.get("/list/:list_name", (req, res) => {
+// 	res.sendFile(`${__dirname}/public/html/list.html`);
+// });
 
 
 app.set("port", (process.env.PORT || 3000));
