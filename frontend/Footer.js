b3ae1d70b5cd74ed6cf5e34d7b7c91a50601e506
Gizzz <bulatkhusainov@yandex.ru>
2017-04-17 22:25:35 +0300

    fix footer text

--- frontend/Footer.js
+++ frontend/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
 	<footer className="page">
-		<a href="https://shop.polymer-project.org/">Inspired by Polymer Demo</a>
+		<a href="https://shop.polymer-project.org/">Inspired by Polymer Demo Project</a>
 		<div className="demo-label">Demo Only</div>
 	</footer>
 );
