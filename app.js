947648ee7a01c5705470ba1d53957c92e2d77cbb
Gizzz <bulatkhusainov@yandex.ru>
2017-04-17 22:36:58 +0300

    remove obsolete route from app.js (express)

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.get("/*", (req, res) => {
 	res.sendFile(`${__dirname}/public/index.html`);
 });
 
-// app.get("/list/:list_name", (req, res) => {
-// 	res.sendFile(`${__dirname}/public/html/list.html`);
-// });
-
 
 app.set("port", (process.env.PORT || 3000));
 
