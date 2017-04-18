22448d7d72772b8541c533e517fe0cc25b1ede6a
Gizzz <bulatkhusainov@yandex.ru>
2017-04-18 23:46:48 +0300

    rewrite List component group
    - extract heading functionality
    - remove ShopItems component

---
+++ frontend/list/Heading.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Heading = (props) => {
+	const category = props.category;
+
+	const categoryName_to_title = {
+		mens_outerwear: "Men's Outerwear",
+		ladies_outerwear: "Ladies Outerwear",
+		mens_tshirts: "Men's T-Shirts",
+		ladies_tshirts: "Ladies T-Shirts",
+	};
+	const categoryTitle = categoryName_to_title[category];
+
+	let itemsCountText = "Loading items...";
+
+	if (props.itemsCount !== null) {
+		itemsCountText = props.itemsCount === 1 
+			? "1 item" 
+			: `${props.itemsCount} items`;
+	}
+
+	return (
+		<div className="heading">
+			<h2>{ categoryTitle }</h2>
+			<span>({ itemsCountText })</span>
+		</div>
+	);
+};
+
+Heading.propTypes = {
+	category: PropTypes.string,
+	// itemsCount: number | null
+	itemsCount: PropTypes.any,
+};
+
+export default Heading;
--- frontend/list/List.js
+++ frontend/list/List.js
@@ -1,17 +1,72 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-import ShopItems from "./ShopItems";
+import Heading from "./Heading";
 
-const List = ({ match }) => (
-	<div className="content list">
-		<div className={ `billboard ${match.params.category}` }></div>
-		<ShopItems category={ match.params.category } />
-	</div>
-);
+class List extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	componentDidMount() {
+		const category = this.props.match.params.category;
+		this.loadData(category);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		const isLocationChanged = nextProps.location !== this.props.location;
+		if (isLocationChanged) {
+			const category = nextProps.match.params.category;
+			this.loadData(category);
+		}
+	}
+
+	loadData(category) {
+		fetch(`/data/${category}.jsons`)
+			.then(response => response.json())
+			.then((json) => {
+				this.setState({ shopItems: json });
+			})
+			.catch(e => console.error(e));
+	}
+
+	render() {
+		const category = this.props.match.params.category;
+
+		let shopItems = this.state.shopItems;
+
+		const isItemsLoaded = shopItems != null;
+		if (isItemsLoaded) {
+			shopItems = shopItems.map((item) => (
+				<li key={item.name}>
+					<Link to={ `/detail/${category}/${item.name}` }>
+						<img src={ item.image } alt="" />
+						<div className="title">{ item.title }</div>
+						<span className="price">${ item.price.toFixed(2) }</span>
+					</Link>
+				</li>
+			));
+		}
+
+		const itemsCount = isItemsLoaded ? shopItems.length : null;
+
+		return (
+			<div className="content list">
+				<div className={ `billboard ${category}` }></div>
+				<Heading category={ category } itemsCount={ itemsCount } />
+				<ul className="items">
+					{ shopItems }
+				</ul>
+			</div>
+		);
+	}
+}
 
 List.propTypes = {
-	match: PropTypes.object
+	match: PropTypes.object,
+	location: PropTypes.object,
 };
 
 export default List;
