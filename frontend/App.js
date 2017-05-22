f48bc81f3759282805c3a920ffef4732985142a3
Gizzz <bulatkhusainov@yandex.ru>
2017-05-22 19:20:54 +0300

    make cart display predefined items; store cart state in App component

--- frontend/App.js
+++ frontend/App.js
@@ -10,6 +10,41 @@ import Detail from "./pages/Detail";
 import Cart from "./pages/Cart";
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			cartItems: [ 
+				{
+					itemData: {
+						"name":"Men+s+Tech+Shell+Full-Zip",
+						"title":"Men's Tech Shell Full-Zip",
+						"category":"mens_outerwear",
+						"price":50.20,
+						"description":"A versatile full-zip that you can wear all day long and even to the gym. This technical shell features moisture-wicking fabric, added stretch and a hidden pocket for your smartphone or media player.&amp;nbsp;&lt;div&gt;&lt;br&gt;&lt;/div&gt;&lt;div&gt;Features:&lt;/div&gt;&lt;div&gt;&lt;ul&gt;&lt;li&gt;100% polyester.&lt;/li&gt;&lt;li&gt;Smooth, technical front with textured mesh back.&lt;/li&gt;&lt;li&gt;Drawstring bottom for adjustable fit.&lt;/li&gt;&lt;li&gt;Raglan sleeves.&lt;/li&gt;&lt;li&gt;Available in forest green with the white Google logo embroidered at left chest.&lt;/li&gt;&lt;/ul&gt;&lt;/div&gt;",
+						"image":"/img/content/10-15068B.jpg",
+						"largeImage":"/img/content/10-15068A.jpg"
+					},
+					quantity: 2,
+					size: "S",
+				},
+				{
+					itemData: {
+						"name":"Anvil+L+S+Crew+Neck+-+Grey",
+						"title":"Anvil L/S Crew Neck - Grey",
+						"category":"mens_outerwear",
+						"price":22.15,
+						"description":"You'll be swooning over this crew neck as soon as you feel how soft it is.&amp;nbsp;&lt;div&gt;&lt;br&gt;&lt;/div&gt;&lt;div&gt;Features:&lt;/div&gt;&lt;div&gt;&lt;ul&gt;&lt;li&gt;40% preshrunk ring-spun cotton, 60% polyester terry fleece.&amp;nbsp;&lt;/li&gt;&lt;li&gt;Available in dark heather charcoal with the white Google logo screen printed across center chest.&lt;/li&gt;&lt;/ul&gt;&lt;/div&gt;",
+						"image":"/img/content/10-14154B.jpg",
+						"largeImage":"/img/content/10-14154A.jpg"
+					},
+					quantity: 4,
+					size: "L",
+				},
+			],
+		};
+	}
+
 	render() {
 		return (
 			<BrowserRouter>
@@ -20,7 +55,7 @@ class App extends React.Component {
 							<Route exact path="/" component={ Home } />
 							<Route path="/list/:category" component={ List } />
 							<Route path="/detail/:category/:itemName" component={ Detail } />
-							<Route path="/cart" component={ Cart } />
+							<Route path="/cart" render={ () => (<Cart items={ this.state.cartItems } />) } />
 						</div>
 					</section>
 					<Footer />
--- frontend/pages/Cart.js
+++ frontend/pages/Cart.js
@@ -1,13 +1,77 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class Cart extends React.Component {
 	render() {
+		const isCartEmpty = !this.props.items;
+		if (isCartEmpty) {
+			return (
+				<div className="content cart">
+					<p className="empty-cart">
+						Your <i className="cart"></i> is empty.
+					</p>
+				</div>
+			);
+		}
+
+		const items = this.props.items;
+		const itemsMarkup = items.map((item) => (
+			<li key={ item.itemData.name }>
+				<div className="left">
+					<Link className="image" to={ `/detail/${item.itemData.category}/${item.itemData.name}` }>
+						<img src={ item.itemData.image } />
+					</Link>
+					<Link className="name" to={ `/detail/${item.itemData.category}/${item.itemData.name}` }>
+						{ item.itemData.title }
+					</Link>
+				</div>
+				<div className="right">
+					<div className="quantity">
+						<label>
+							Qty:
+							<select name="quantity" defaultValue={ item.quantity }>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+							</select>
+						</label>
+					</div>
+					<div className="size">
+						Size: <span>{ item.size }</span>
+					</div>
+					<div className="price">${ item.itemData.price.toFixed(2) }</div>
+					<button className="delete"></button>
+				</div>
+			</li>
+		));
+
+		const totalPrice = items.reduce((sum, item) => {
+			return sum + item.itemData.price * item.quantity;
+		}, 0);
+
 		return (
 			<div className="content cart">
-				<h1>Cart</h1>
+				<div className="heading">
+					<h1>Your Cart</h1>
+					<span>(2 items)</span>
+				</div>
+				<ul className="items">
+					{ itemsMarkup }
+				</ul>
+				<div className="checkout-box">
+					Total: <span className="subtotal">${ totalPrice.toFixed(2) }</span>
+					<a className="btn" href="#">checkout</a>
+				</div>
 			</div>
 		);
 	}
 }
+
+Cart.propTypes = {
+	items: PropTypes.array,
+};
 
 export default Cart;
