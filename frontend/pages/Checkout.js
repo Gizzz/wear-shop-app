3dc5a4628c957b606eb20b9f3afc04cb0290ee54
Gizzz <bulatkhusainov@yandex.ru>
2017-06-08 20:10:28 +0300

    checkout page: markup stage finished

--- frontend/pages/Checkout.js
+++ frontend/pages/Checkout.js
@@ -12,9 +12,19 @@ const styles = {
 	underlineFocusStyle: {
 		borderColor: "#202020",
 	},
+	selectedMenuItemStyle: {
+		fontWeight: "bold", 
+		color: "#000",
+	}
 };
 
 class Checkout extends React.Component {
+	state = {
+		value: 1,
+	}
+
+	handleChange = (event, index, value) => this.setState({value})
+
 	render() {
 		return (
 			<div className="content checkout">
@@ -25,81 +35,184 @@ class Checkout extends React.Component {
 				<div className="checkout-form row">
 					<section className="left">
 						<h2>Account Information</h2>
-						<TextField
-							fullWidth
-							floatingLabelText="Email"
-							floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
-							underlineFocusStyle={ styles.underlineFocusStyle }
-						/>
-						<br />
-						<TextField
-							fullWidth
-							floatingLabelText="Phone Number"
-							floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
-							underlineFocusStyle={ styles.underlineFocusStyle }
-						/>
-						
-						<h2>Shipping Address</h2>
-						<TextField
-							fullWidth
-							floatingLabelText="Address"
-							floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
-							underlineFocusStyle={ styles.underlineFocusStyle }
-						/>
-						<TextField
-							fullWidth
-							floatingLabelText="City"
-							floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
-							underlineFocusStyle={ styles.underlineFocusStyle }
-						/>
-						<div className="row">
-							<div className="col half-width">
-								<TextField
-									fullWidth
-									floatingLabelText="State/Province"
-									floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
-									underlineFocusStyle={ styles.underlineFocusStyle }
-								/>
-							</div>
-							<div className="col half-width">
-								<TextField
-									fullWidth
-									floatingLabelText="Zip/Postal Code"
-									floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
-									underlineFocusStyle={ styles.underlineFocusStyle }
-								/>
-							</div>
+						<div className="accout-information">
+							<TextField
+								fullWidth
+								floatingLabelText="Email"
+								floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
+								underlineFocusStyle={ styles.underlineFocusStyle }
+							/>
+							<br />
+							<TextField
+								fullWidth
+								floatingLabelText="Phone Number"
+								floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
+								underlineFocusStyle={ styles.underlineFocusStyle }
+							/>
 						</div>
-						<SelectField
-							fullWidth
-							floatingLabelText="Frequency"
-						>
-							<MenuItem value={1} primaryText="Never" />
-							<MenuItem value={2} primaryText="Every Night" />
-						</SelectField>
-
+						<h2>Shipping Address</h2>
+						<div className="shipping-address">
+							<TextField
+								fullWidth
+								floatingLabelText="Address"
+								floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+								underlineFocusStyle={styles.underlineFocusStyle}
+							/>
+							<TextField
+								fullWidth
+								floatingLabelText="City"
+								floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+								underlineFocusStyle={styles.underlineFocusStyle}
+							/>
+							<div className="row">
+								<div className="col half-width">
+									<TextField
+										fullWidth
+										floatingLabelText="State/Province"
+										floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+										underlineFocusStyle={styles.underlineFocusStyle}
+									/>
+								</div>
+								<div className="col half-width">
+									<TextField
+										fullWidth
+										floatingLabelText="Zip/Postal Code"
+										floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+										underlineFocusStyle={styles.underlineFocusStyle}
+									/>
+								</div>
+							</div>
+							<SelectField
+								fullWidth
+								floatingLabelText="Country"
+								selectedMenuItemStyle={styles.selectedMenuItemStyle}
+								underlineFocusStyle={styles.underlineFocusStyle}
+								value={this.state.value}
+								onChange={this.handleChange}
+							>
+								<MenuItem value={1} primaryText="United States" />
+								<MenuItem value={2} primaryText="Canada" />
+							</SelectField>
+						</div>
 						<h2>Billing Address</h2>
-						<Checkbox
-							label="Use different billing address"
-							style={{ marginTop: "29px" }}
-							iconStyle={{ fill: "#202020" }}
-						/>
+						<div className="billing-address">
+							<Checkbox
+								label="Use different billing address"
+								style={{ marginTop: "29px" }}
+								iconStyle={{ fill: "#202020" }}
+							/>
+							<TextField
+								fullWidth
+								floatingLabelText="Address"
+								floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+								underlineFocusStyle={styles.underlineFocusStyle}
+							/>
+							<TextField
+								fullWidth
+								floatingLabelText="City"
+								floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+								underlineFocusStyle={styles.underlineFocusStyle}
+							/>
+							<div className="row">
+								<div className="col half-width">
+									<TextField
+										fullWidth
+										floatingLabelText="State/Province"
+										floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+										underlineFocusStyle={styles.underlineFocusStyle}
+									/>
+								</div>
+								<div className="col half-width">
+									<TextField
+										fullWidth
+										floatingLabelText="Zip/Postal Code"
+										floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+										underlineFocusStyle={styles.underlineFocusStyle}
+									/>
+								</div>
+							</div>
+							<SelectField
+								fullWidth
+								floatingLabelText="Country"
+								selectedMenuItemStyle={styles.selectedMenuItemStyle}
+								underlineFocusStyle={styles.underlineFocusStyle}
+								value={this.state.value}
+								onChange={this.handleChange}
+							>
+								<MenuItem value={1} primaryText="United States" />
+								<MenuItem value={2} primaryText="Canada" />
+							</SelectField>
+						</div>
 					</section>
 					<section className="right">
 						<h2>Payment Method</h2>
-						<TextField
-							fullWidth
-							floatingLabelText="Cardholder Name"
-							floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
-							underlineFocusStyle={ styles.underlineFocusStyle }
-						/>
-						<br />
-						<TextField
-							fullWidth
-							floatingLabelText="Card Number"
-							floatingLabelFocusStyle={ styles.floatingLabelFocusStyle }
-							underlineFocusStyle={ styles.underlineFocusStyle }
-						/>
+						<div className="payment-method">
+							<TextField
+								fullWidth
+								floatingLabelText="Cardholder Name"
+								floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+								underlineFocusStyle={styles.underlineFocusStyle}
+							/>
+							<br />
+							<TextField
+								fullWidth
+								floatingLabelText="Card Number"
+								floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+								underlineFocusStyle={styles.underlineFocusStyle}
+							/>
+							<div className="row">
+								<div className="col third-width">
+									<SelectField
+										fullWidth
+										floatingLabelText="Expiry Month"
+										selectedMenuItemStyle={styles.selectedMenuItemStyle}
+										underlineFocusStyle={styles.underlineFocusStyle}
+										value={this.state.value}
+										onChange={this.handleChange}
+									>
+										<MenuItem value={1} primaryText="Jan" />
+										<MenuItem value={2} primaryText="Feb" />
+									</SelectField>
+								</div>
+								<div className="col third-width">
+									<SelectField
+										fullWidth
+										floatingLabelText="Expiry Year"
+										selectedMenuItemStyle={styles.selectedMenuItemStyle}
+										underlineFocusStyle={styles.underlineFocusStyle}
+										value={this.state.value}
+										onChange={this.handleChange}
+									>
+										<MenuItem value={1} primaryText="2016" />
+										<MenuItem value={2} primaryText="2017" />
+									</SelectField>
+								</div>
+								<div className="col third-width">
+									<TextField
+										fullWidth
+										floatingLabelText="CVV"
+										floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+										underlineFocusStyle={styles.underlineFocusStyle}
+									/>
+								</div>
+							</div>
+						</div>
+						<h2>Order Summary</h2>
+						<div className="order-summary">
+							<div className="row">
+								<div className="flex">Men's Tech Shell Full-Zip</div>
+								<div>$50.20</div>
+							</div>
+							<div className="row">
+								<div className="flex">Men's Tech Shell Full-Zip</div>
+								<div>$50.20</div>
+							</div>
+							<div className="row total">
+								<div className="flex">Total</div>
+								<div>$72.35</div>
+							</div>
+						</div>
+						<button className="btn">Place Order</button>
 					</section>
 				</div>
 			</div>
