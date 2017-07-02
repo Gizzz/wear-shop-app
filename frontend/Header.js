f369ca8d72636e84f4133ed9834d2df1f595879f
Gizzz <bulatkhusainov@yandex.ru>
2017-07-02 23:02:57 +0300

    Header.js: make scroll to top when location changes

--- frontend/Header.js
+++ frontend/Header.js
@@ -47,6 +47,12 @@ class Header extends React.Component {
 		}
 	}
 
+	componentDidUpdate(prevProps) {
+		if (this.props.location.pathname !== prevProps.location.pathname) {
+			window.scrollTo(0, 0);
+		}
+	}
+
 	handleTabClick = (newValue) => {
 		this.setState({ selectedTabValue: newValue });
 
