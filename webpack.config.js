c0e80fe6608b9e522b255102d4f2b82c8acbb292
Gizzz <bulatkhusainov@yandex.ru>
2017-07-19 13:09:16 +0300

    move /frontend to /src folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/index.js',
+  entry: './src/frontend/index.js',
   devtool: 'source-map',
   output: {
     path: path.join(__dirname, '/public'),
