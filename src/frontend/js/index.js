e2a51242d6deae203e2f1856a02ab7006250ab95
Gizzz <bulatkhusainov@yandex.ru>
2017-08-29 22:58:41 +0300

    redux: add initial redux support

--- src/frontend/js/index.js
+++ src/frontend/js/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import App from './components/App'
 
@@ -11,9 +13,14 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
+const reducer = () => {}
+const store = createStore(reducer)
+
 ReactDOM.render(
   <MuiThemeProvider>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </MuiThemeProvider>, 
-  document.getElementById('root')
+  document.getElementById('root'),
 )
