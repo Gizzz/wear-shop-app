a2ac7532a5b4c5a1e950cb552bfcf7ca89733989
Gizzz <bulatkhusainov@yandex.ru>
2017-08-31 19:35:31 +0300

    redux: add thunk and logger middlewares

--- src/frontend/js/index.js
+++ src/frontend/js/index.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-
-import App from './components/App'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+
+import App from './components/App'
 
 // from material-ui docs:
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
+const middlewares = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(createLogger())
+}
+
 const reducer = () => {}
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  applyMiddleware(...middlewares),
+)
 
 ReactDOM.render(
   <MuiThemeProvider>
