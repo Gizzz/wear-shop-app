ee652cf27a126acfae012740a10e326cdf7c9270
Gizzz <bulatkhusainov@yandex.ru>
2017-10-13 20:54:46 +0300

    redux: update shop-items reducer to  use normalized state (byId, allIds)

--- src/frontend/js/redux/reducers/shop-items.js
+++ src/frontend/js/redux/reducers/shop-items.js
@@ -1,39 +1,57 @@
+import { combineReducers } from 'redux'
 import * as actionTypes from '../action-types'
 
-const initialState = {
-  itemsByCategory: {
-    mens_outerwear: [],
-    ladies_outerwear: [],
-    mens_tshirts: [],
-    ladies_tshirts: [],
-  },
-}
-
-function shopItems(state = initialState, action) {
+function byId(state = {}, action) {
   switch (action.type) {
-    case actionTypes.LOAD_SHOP_ITEMS__SUCCESS:
+    case actionTypes.LOAD_SHOP_ITEMS__SUCCESS: {
+      const itemsByName = {}
+      action.result.forEach((item) => {
+        itemsByName[item.name] = item
+      })
+
       return {
         ...state,
-        itemsByCategory: {
-          ...state.itemsByCategory,
-          [action.category]: action.result,
-        },
+        ...itemsByName,
       }
+    }
     default:
       return state
   }
 }
 
+function allIds(state = [], action) {
+  switch (action.type) {
+    case actionTypes.LOAD_SHOP_ITEMS__SUCCESS: {
+      const itemNames = [ ...state ]
+      action.result.forEach((item) => {
+        if (!itemNames.includes(item.name)) {
+          itemNames.push(item.name)
+        }
+      })
+
+      return [ ...itemNames ]
+    }
+    default:
+      return state
+  }
+}
+
+const shopItems = combineReducers({ byId, allIds })
 export default shopItems
 
 // selectors
 
 export function getShopItemsByCategory(state, category) {
-  return state.itemsByCategory[category]
+  const allItems = state.allIds.map(id => state.byId[id])
+  const itemsInCategory = allItems.filter((item) => { return item.category === category })
+
+  return itemsInCategory
 }
 
 export function getShopItem(state, category, name) {
-  const itemsInCategory = state.itemsByCategory[category]
+  const allItems = state.allIds.map(id => state.byId[id])
+  const itemsInCategory = allItems.filter((item) => { return item.category === category })
   const item = itemsInCategory.find(item => item.name === name)
+
   return item
 }
