a57493f05e7e893807b277dbe3b8d64e21910f81
Gizzz <bulatkhusainov@yandex.ru>
2017-10-14 14:51:56 +0300

    webpack: switch to 'env' babel preset in production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -28,7 +28,7 @@ module.exports = {
           'babelrc': false,
           'presets': [
             ['react'],
-            ['es2015', { 'modules': false }],
+            ['env'],
           ],
           'plugins': [
             'transform-class-properties',
