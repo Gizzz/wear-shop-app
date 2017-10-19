2154bb28de289890e5de886e8925275f784dd42a
Gizzz <bulatkhusainov@yandex.ru>
2017-10-19 17:25:18 +0300

    redux: CartContainer.js - load all related shop items before rendering

--- src/frontend/js/components/pages/cart/CartContainer.js
+++ src/frontend/js/components/pages/cart/CartContainer.js
@@ -1,8 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import * as actionCreators from '../../../redux/action-creators'
 import { selectors } from '../../../redux/reducers'
 import Cart from './Cart'
+
+class CartWrapper extends React.Component {
+  static propTypes = {
+    shopItems: PropTypes.array.isRequired,
+    cartEntries: PropTypes.array.isRequired,
+    loadShopItems: PropTypes.func.isRequired,
+  }
+
+  componentDidMount() {
+    const shopItems = this.props.shopItems
+    const cartEntries = this.props.cartEntries
+
+    let isRelatedShopItemsLoaded = true
+
+    cartEntries.forEach((entry) => {
+      const isShopItemLoaded = shopItems.find(item => item.id === entry.shopItemId) !== undefined
+      if (!isShopItemLoaded) {
+        isRelatedShopItemsLoaded = false
+      }
+    })
+
+    if (!isRelatedShopItemsLoaded) {
+      this.props.loadShopItems()
+    }
+  }
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    const { loadShopItems: notUsed, ...rest } = this.props
+
+    return (
+      <Cart {...rest} />
+    )
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -14,7 +51,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = {
   onQuantityChange: actionCreators.updateCartEntry_setQuantity,
   onRemove: actionCreators.removeCartEntry,
+  loadShopItems: actionCreators.loadShopItems
 }
 
-const CartContainer = connect(mapStateToProps, mapDispatchToProps)(Cart)
+const CartContainer = connect(mapStateToProps, mapDispatchToProps)(CartWrapper)
 export default CartContainer
